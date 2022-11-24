ea6e2c2d67da8acb176a18e606ec819c396647b2
Alexey Belyaev <lebelr@yandex.ru>
2022-11-24 16:52:52 +0500

    Исправление названий в скриптах

--- scripts/index.js
+++ scripts/index.js
@@ -1,67 +1,40 @@
 const profileEdit = document.querySelector('.profile__edit-button'); //Находим кнопку редактирования
-const popupEdit = document.querySelector('.popup_type_edit'); //Находим кнопку попап
-const popupCloseEdit = popupEdit.querySelector('.popup__close_type_edit'); //Находим кнопку закрытия
-const popupNameEdit = popupEdit.querySelector('.popup__name_type_edit'); //Находим поле ввода имени в попапе
+const popupEditProfile = document.querySelector('.popup_type_edit'); //Находим кнопку попап
+const popupCloseEdit = popupEditProfile.querySelector('.popup__close_type_edit'); //Находим кнопку закрытия
+const popupNameEdit = popupEditProfile.querySelector('.popup__name_type_edit'); //Находим поле ввода имени в попапе
 const profileName = document.querySelector('.profile__name'); //Находим имя профиля на странице
 
-const popupAboutEdit = popupEdit.querySelector('.popup__about_type_edit'); //Находим поле ввода о себе в попапе
+const popupAboutEdit = popupEditProfile.querySelector('.popup__about_type_edit'); //Находим поле ввода о себе в попапе
 const profileAbout = document.querySelector('.profile__about'); //Находим о себе профиля на странице
 
 // Функции для открытия и закрытия попапов
 
-const popupOpen = (popup) => {
+const openPopup = (popup) => {
   popup.classList.add('popup_opened');
 }
 
-const popupClose = (popup) => {
+const closePopup = (popup) => {
   popup.classList.remove('popup_opened');
 }
 
 profileEdit.addEventListener('click', () => { //Команда по клику для кнопки редактирования
-  popupOpen(popupEdit); //Добавление класса попапу, чтобы он появился на странице
+  openPopup(popupEditProfile); //Добавление класса попапу, чтобы он появился на странице
   popupNameEdit.value = profileName.textContent; //Команда, которая заполняет поле ввода имени в попапе именем профиля на странице
   popupAboutEdit.value = profileAbout.textContent; //Команда, которая заполняетполе ввода о себе в попапе тем, что содержится в "о себе" профиля на странице
 });
 
 popupCloseEdit.addEventListener('click', () => { //Команда по клику для кнопки закрытия
-  popupClose(popupEdit); //Удаление класса попапу, чтобы он закрылся на странице
+  closePopup(popupEditProfile); //Удаление класса попапу, чтобы он закрылся на странице
 });
 
-function submitHandleEdit(evt) { //Функция для кнопки сохранения
+function submitEditProfile(evt) { //Функция для кнопки сохранения
   evt.preventDefault(); //Отмена свойств
   profileName.textContent = popupNameEdit.value; //Команда, которая будет заменять имя профиля текстом из поля ввода имени в попапе
   profileAbout.textContent = popupAboutEdit.value; //Команда, которая будет заменять "о себе" текстом из поля ввода "о себе" в попапе
-  popupClose(popupEdit); //Удаление класса попапу, чтобы он закрылся на странице
+  closePopup(popupEditProfile); //Удаление класса попапу, чтобы он закрылся на странице
 };
 
-popupEdit.addEventListener('submit', submitHandleEdit); //Команда по клику для кнопки сохранения
-
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
+popupEditProfile.addEventListener('submit', submitEditProfile); //Команда по клику для кнопки сохранения
 
 // Команда для поиска блока с карточками и шаблона карточки
 
@@ -92,9 +65,7 @@ const popupImageTitle = document.querySelector('.popup__image-title');
 // Функция для закрытия попапа
 
 popupCloseImage.addEventListener('click', () => {
-  popupClose(popupTypeImage);
-  popupImage.src = '';
-  popupImage.alt = '';
+  closePopup(popupTypeImage);
 });
 
 // Функция, в которой клонируется карточка, заполняется содержимым из массива, добавляются команды: кнопке удаления, кнопке лайка, открытие попапа при нажатии на картинку
@@ -117,7 +88,7 @@ const generateCard = (dataCard) => {
   const cardImage = cardItem.querySelector('.cards__image');
   cardImage.addEventListener('click', (evt) => {
     evt.preventDefault();
-    popupOpen(popupTypeImage);
+    openPopup(popupTypeImage);
     popupImage.src = cardImage.src;
     popupImage.alt = dataCard.name;
     popupImageTitle.textContent = dataCard.name;
@@ -138,25 +109,25 @@ initialCards.forEach((dataCard) => {
 
 // Команды для поиска кнопки добавления карточки, попапа, кнопки закрытия попапа
 
-const addButton = document.querySelector('.profile__add-button');
-const popupAdd = document.querySelector('.popup_type_add');
+const buttonOpenAddCardPopup = document.querySelector('.profile__add-button');
+const popupAddCard = document.querySelector('.popup_type_add');
 const popupCloseAdd = document.querySelector('.popup__close_type_add');
 
 // Команды для поиска полей ввода попапа
 
-const popupNameAdd = popupAdd.querySelector('.popup__name_type_add');
-const popupAboutAdd = popupAdd.querySelector('.popup__about_type_add');
+const inputCardName = popupAddCard.querySelector('.popup__name_type_add');
+const inputCardLink = popupAddCard.querySelector('.popup__about_type_add');
+const popupFormAdd = popupAddCard.querySelector('.popup__form');
 
 // Команды по нажитию кнопок для открытия и закрытия попапа
 
-addButton.addEventListener('click', () => {
-  popupOpen(popupAdd);
+buttonOpenAddCardPopup.addEventListener('click', () => {
+  openPopup(popupAddCard);
+  popupFormAdd.reset();
 });
 
 popupCloseAdd.addEventListener('click', () => {
-  popupClose(popupAdd);
-  popupAboutAdd.value = '';
-  popupNameAdd.value = '';
+  closePopup(popupAddCard);
 });
 
 // Функция для добавления карточки и ее заполнения
@@ -167,10 +138,8 @@ const addCard = (dataCard) => {
 
 const handleAddCard = (evt) => {
   evt.preventDefault();
-  addCard({ name: popupNameAdd.value, link: popupAboutAdd.value });
-  popupClose(popupAdd);
-  popupAboutAdd.value = '';
-  popupNameAdd.value = '';
+  addCard({ name: inputCardName.value, link: inputCardLink.value });
+  closePopup(popupAddCard);
 };
 
-popupAdd.addEventListener('submit', handleAddCard);
+popupAddCard.addEventListener('submit', handleAddCard);
